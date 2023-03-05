325fb5247cb62d5ef3a956135cd2146f2bcf7208
guma2k2 <102044008+guma2k2@users.noreply.github.com>
2023-03-05 09:14:15 +0700

    Update KhachHangDAO.java

--- src/main/java/com/nhom13/DAO/KhachHangDAO.java
+++ src/main/java/com/nhom13/DAO/KhachHangDAO.java
@@ -1,7 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.nhom13.DAO;
 
 import com.nhom13.Database.DatabaseHelper;
-import com.nhom13.Entity.Ban;
+import com.nhom13.Entity.KhachHang;
 import com.nhom13.Entity.KhuyenMai;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,35 +17,34 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class KhuyenMaiDAO {
+/**
+ *
+ * @author thuan
+ */
+public class KhachHangDAO {
 
-    public KhuyenMaiDAO() {
-    }
-
-    public static String DateToString(Date date) {
-
+    public String DateToString(Date date) {
         DateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
         return date_format.format(date);
     }
 
-    public List<KhuyenMai> findAll() {
-        List<KhuyenMai> result = new ArrayList<>();
+    public List<KhachHang> findAll() {
+        List<KhachHang> result = new ArrayList<>();
         Connection con = null;
         Statement statement = null;
         try {
             con = DatabaseHelper.openConnection();
             statement = con.createStatement();
-            String sql = "SELECT * FROM KHUYENMAI ";
+            String sql = "SELECT * FROM KHACHHANG ";
             ResultSet resultset = statement.executeQuery(sql);
             while (resultset.next()) {
-                KhuyenMai khuyenMai = new KhuyenMai();
-                khuyenMai.setId(resultset.getInt(1));
-                khuyenMai.setNgayApDung(resultset.getDate(2));
-                khuyenMai.setNgayKetThuc(resultset.getDate(3));
-                khuyenMai.setGiaTri(resultset.getInt(4));
-                khuyenMai.setLyDo(resultset.getString(5));
-                khuyenMai.setMaNv(resultset.getString(6));
-                result.add(khuyenMai);
+                KhachHang khachHang = new KhachHang();
+                khachHang.setId(resultset.getInt(1));
+                khachHang.setHo(resultset.getString(2));
+                khachHang.setTen(resultset.getString(3));
+                khachHang.setNgaySinh(resultset.getDate(4));
+                khachHang.setSdt(resultset.getString(5));
+                result.add(khachHang);
             }
 
         } catch (Exception ex) {
@@ -50,79 +53,78 @@ public class KhuyenMaiDAO {
         return result;
     }
 
-    public void save(KhuyenMai sale, String manv) {
+    public void save(KhachHang temp) {
         Connection con = null;
         PreparedStatement statement = null;
         try {
             con = DatabaseHelper.openConnection();
-            // list ra atribute 
-            // query 
-            String sql = "INSERT INTO KHUYENMAI(NGAY_AP_DUNG, NGAY_KET_THUC , PHAN_TRAM_KM , LY_DO_KM , MA_NV) VALUES (? , ? , ? ,?,?)";
-            // statement.prepareCall...
+            String ho = temp.getHo();
+            String ten = temp.getTen();
+            String sdt = temp.getSdt();
+            Date ns = temp.getNgaySinh();
+
+            String sql = "INSERT INTO KHACHHANG( HO, TEN, SDT, NGAY_SINH)"
+                    + "VALUES(? , ? , ? ,? )";
             statement = con.prepareCall(sql);
-            // set atributes for statement
-            statement.setString(1, DateToString(sale.getNgayApDung()));
-            statement.setString(2, DateToString(sale.getNgayKetThuc()));
-            statement.setInt(3, sale.getGiaTri());
-            statement.setString(4, sale.getLyDo());
-            statement.setString(5, manv);
-            // statement.executeQuery
+            statement.setString(1, ho);
+            statement.setString(2, ten);
+            statement.setString(3, sdt);
+            statement.setString(4, DateToString(ns));
             statement.executeUpdate();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 
-    public void updateSale(KhuyenMai sale, String manv) {
+    public void update(KhachHang client) {
         try {
             Connection con = DatabaseHelper.openConnection();
             PreparedStatement statement = null;
-            String sql = "UPDATE KHUYENMAI SET  NGAY_AP_DUNG=? , NGAY_KET_THUC = ? , PHAN_TRAM_KM = ? , LY_DO_KM = ? , MA_NV =? WHERE ID_KM = ?";
+            String sql = "UPDATE KHACHHANG SET  HO = ? , TEN = ? , SDT = ? , NGAY_SINH = ? WHERE ID_KH = ?";
             statement = con.prepareCall(sql);
-            statement.setInt(6, sale.getId());
-            statement.setString(1, DateToString(sale.getNgayApDung()));
-            statement.setString(2, DateToString(sale.getNgayKetThuc()));
-            statement.setInt(3, sale.getGiaTri());
-            statement.setString(4, sale.getLyDo());
-            statement.setString(5, manv);
+            statement.setInt(5, client.getId());
+            statement.setString(1, client.getHo());
+            statement.setString(2, client.getTen());
+            statement.setString(3, client.getSdt());
+            statement.setString(4, DateToString(client.getNgaySinh()));
             statement.executeUpdate();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public KhuyenMai findById(int id) {
+    public KhachHang findById(int id) {
         Connection con = null;
         Statement statement = null;
-        KhuyenMai khuyenMai = new KhuyenMai();
+        KhachHang khachHang = null;
         try {
             con = DatabaseHelper.openConnection();
             statement = con.createStatement();
-            String sql = "SELECT * FROM KHUYENMAI WHERE ID_KM = " + id;
+            String sql = "SELECT * FROM KHACHHANG  WHERE ID_KH = " + id;
             ResultSet resultset = statement.executeQuery(sql);
             while (resultset.next()) {
-                khuyenMai = new KhuyenMai();
-                khuyenMai.setId(resultset.getInt(1));
-                khuyenMai.setNgayApDung(resultset.getDate(2));
-                khuyenMai.setNgayKetThuc(resultset.getDate(3));
-                khuyenMai.setGiaTri(resultset.getInt(4));
-                khuyenMai.setLyDo(resultset.getString(5));
-                khuyenMai.setMaNv(resultset.getString(6));
+                khachHang = new KhachHang();
+                khachHang.setId(resultset.getInt(1));
+                khachHang.setHo(resultset.getString(2));
+                khachHang.setTen(resultset.getString(3));
+                khachHang.setSdt(resultset.getString(4));
+                khachHang.setNgaySinh(resultset.getDate(5));
+
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return khuyenMai;
+        return khachHang;
     }
 
-    public void deleteKhuyenMai(KhuyenMai khuyenMai) {
+    public void deleteKhachHang(KhachHang khachHang) {
         PreparedStatement state = null;
         Connection con = null;
         try {
             con = DatabaseHelper.openConnection();
-            int id = khuyenMai.getId();
-            String sql = "DELETE FROM KHUYENMAI WHERE ID_KM = " + id;
+            int id = khachHang.getId();
+            String sql = "DELETE FROM KHACHHANG WHERE ID_KH = " + id;
             state = con.prepareCall(sql);
             state.executeUpdate();
 
@@ -131,39 +133,23 @@ public class KhuyenMaiDAO {
         }
     }
     
-    public KhuyenMai getKMInTime()throws Exception{
-        KhuyenMai km;
-        String sql = "SELECT ID_KM, PHAN_TRAM_KM FROM KHUYENMAI "
-                + "WHERE DATEDIFF(DAY,GETDATE(),NGAY_AP_DUNG)<0 AND DATEDIFF(DAY,GETDATE(),NGAY_KET_THUC)>0";
-        try(Connection con = DatabaseHelper.openConnection();
-            Statement statement = con.createStatement();
-            ResultSet result = statement.executeQuery(sql)){
-            while(result.next()){
-                km = new KhuyenMai(result.getInt(1),result.getInt(2));
-                return km;
-            }
-        }
-        return null;
-    }
-    
-    public List<KhuyenMai> searchByDate(String date) {
-        List<KhuyenMai> result = new ArrayList<>();
+    public List<KhachHang> searchByName(String keyword) {
+        List<KhachHang> result = new ArrayList<>();
         Connection con = null;
         Statement statement = null;
         try {
             con = DatabaseHelper.openConnection();
             statement = con.createStatement();
-            String sql = "SELECT * FROM KHUYENMAI KM WHERE KM.NGAY_AP_DUNG <=  '"+date+"'AND KM.NGAY_KET_THUC >= '"+date+"' ";
+            String sql = "SELECT * FROM KHACHHANG K WHERE CONCAT(K.HO, ' ' ,K.TEN) LIKE '%"+ keyword +"%' ";
             ResultSet resultset = statement.executeQuery(sql);
             while (resultset.next()) {
-                KhuyenMai khuyenMai = new KhuyenMai();
-                khuyenMai.setId(resultset.getInt(1));
-                khuyenMai.setNgayApDung(resultset.getDate(2));
-                khuyenMai.setNgayKetThuc(resultset.getDate(3));
-                khuyenMai.setGiaTri(resultset.getInt(4));
-                khuyenMai.setLyDo(resultset.getString(5));
-                khuyenMai.setMaNv(resultset.getString(6));
-                result.add(khuyenMai);
+                KhachHang khachHang = new KhachHang();
+                khachHang.setId(resultset.getInt(1));
+                khachHang.setHo(resultset.getString(2));
+                khachHang.setTen(resultset.getString(3));
+                khachHang.setNgaySinh(resultset.getDate(4));
+                khachHang.setSdt(resultset.getString(5));
+                result.add(khachHang);
             }
 
         } catch (Exception ex) {
@@ -171,6 +157,4 @@ public class KhuyenMaiDAO {
         }
         return result;
     }
-    
-    
 }
